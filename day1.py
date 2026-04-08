d16d27df127fae5dfe0c3405bf03ff618319308b
John Carl <johncarlsantos30@gmail.com>
2026-04-08 18:56:57 +0800

    Add day 1 code and scraper

---
+++ day1.py
@@ -0,0 +1,54 @@
+import os
+from scraper import fetch_website_contents
+from openai import OpenAI
+
+OLLAMA_BASE_URL = "http://localhost:11434/v1"
+MODEL = "llama3.2"
+
+
+system_prompt = """
+You are a snarky assistant that analyzes the contents of a website,
+and provides a short, snarky, humorous summary, ignoring text that might be navigation related.
+Respond in markdown. Do not wrap the markdown in a code block - respond just with the markdown.
+"""
+
+user_prompt_prefix = """
+Here are the contents of a website.
+Provide a short summary of this website.
+If it includes news or announcements, then summarize these too.
+
+"""
+messages = [
+    {"role": "system", "content": "You are a helpful assistant"},
+    {"role": "user", "content": "What is 2 + 2?"}
+]
+
+response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+response.choices[0].message.content
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_prefix + website}
+    ]
+
+def summarize(url):
+    
+    ollama = OpenAI(base_url=OLLAMA_BASE_URL, api_key='ollama')
+    website = fetch_website_contents(url)
+    response = ollama.chat.completions.create(
+        model=MODEL,
+        messages=messages_for(website)
+    )
+    return response.choices[0].message.content
+
+def main():
+    
+    url = input("Enter a URL to summarize: ")
+    print("\nFetching and summarizing...\n")
+    summary = summarize(url)
+    print(summary)
+
+
+if __name__ == "__main__":
+    main()
